201e8b48a91074ad0513248f53c8496138bbccf0
unknown <osama_magdy0@yahoo.com>
2022-12-07 14:41:27 +0200

    the final javascript file from my home laptop

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const allColorEl = document.querySelectorAll("span");
 const bigBoxEl = document.querySelector(".sample");
-// console.log(bigBoxEl);
 
 if (window.localStorage.getItem("color")) {
   bigBoxEl.style.backgroundColor = window.localStorage.getItem("color");
-} else {
-  console.log(`no`);
+  allColorEl.forEach((co) => {
+    co.classList.remove("active");
+  });
+  document
+    .querySelector(`[data-color="${window.localStorage.getItem("color")}"]`)
+    .classList.add("active");
 }
 allColorEl.forEach((col) => {
-  //   col.classList.remove("active");
   col.addEventListener("click", (e) => {
     allColorEl.forEach((co) => {
       co.classList.remove("active");
     });
     col.classList.add("active");
-    console.log(e.currentTarget.dataset.color);
     window.localStorage.setItem("color", e.currentTarget.dataset.color);
     bigBoxEl.style.backgroundColor = e.currentTarget.dataset.color;
   });
